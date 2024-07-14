4bed36ed597d8561596ff4e62a050aa931511505
karthik <karthvc@gmail.com>
2024-07-14 15:01:19 +0100

    intial commit

---
+++ src/main/java/com/ecom/paype/PaypeApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.paype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/paype/PaypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.paype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
